9d27c02b4b548e0573a67cbde9e4b3b1d9725b8a
Luis Mateo Guerra Arango <arangoluismateo@gmail.com>
2026-07-22 16:36:14 -0500

    react

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+const fecha = new Date().toLocaleString();
+return (
+<main>
+<h1>Hola SENA</h1>
+<p>esta es mi primera clase de react, en la cual estoyt aprndiendo mucho, ma llamo luis mateo un gusto</p>
+<img src="https://www.bing.com/th/id/OIP.Mg8OtQhtFFJT1KIQHZMewgHaJQ?w=111&h=128&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=ImgAns&rm=2" alt="Imagen de ejemplo" />
+</main>
+)
+}
